7397f710ce6d6e0af3d9f4e9c8a0db935dddd28d
prashant-yubi <prashant.kumar@go-yubi.com>
2023-01-03 14:21:34 +0530

    db upgrade (29) for doc type, sub type and tags

---
+++ docs-core/src/main/resources/db/update/dbupdate-029-0.sql
@@ -0,0 +1,4 @@
+CREATE TABLE tag_type(id serial PRIMARY KEY, name VARCHAR(128) UNIQUE NOT NULL);
+CREATE TABLE all_tags(id serial PRIMARY KEY, tag_id VARCHAR NOT NULL, tag_name VARCHAR UNIQUE NOT NULL, tag_type_id INT, FOREIGN KEY(tag_id) REFERENCES t_tag(tag_id_c), FOREIGN KEY(tag_type_id) REFERENCES tag_type(id));
+CREATE TABLE doc_type (id serial PRIMARY KEY, "key" VARCHAR(128) UNIQUE NOT NULL, label VARCHAR(128));
+CREATE TABLE doc_subtype (id serial PRIMARY KEY, doc_type_id INT NOT NULL, "key" VARCHAR(128) NOT NULL, label VARCHAR(128), FOREIGN KEY(doc_type_id) REFERENCES doc_type (id));
