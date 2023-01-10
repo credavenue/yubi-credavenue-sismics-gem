7353cf60545121d01b290cac1e7a3ff0dd905c48
prashant <118874032+prashant-yubi@users.noreply.github.com>
2023-01-10 12:10:11 +0530

    Dtrack issue fixes and package upgrades (#8)
    * removed unused twelvemonkeys packages
    
    * h2database package removed
    
    * package upgrade
    
    * log4j and subethamail package removed
    
    * package update
    
    * file size in file version api

--- docs-web-common/src/test/java/com/sismics/docs/rest/BaseJerseyTest.java
+++ docs-web-common/src/test/java/com/sismics/docs/rest/BaseJerseyTest.java
@@ -15,8 +15,8 @@ import org.glassfish.jersey.test.spi.TestContainerException;
 import org.glassfish.jersey.test.spi.TestContainerFactory;
 import org.junit.After;
 import org.junit.Before;
-import org.subethamail.wiser.Wiser;
-import org.subethamail.wiser.WiserMessage;
+//import org.subethamail.wiser.Wiser;
+//import org.subethamail.wiser.WiserMessage;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -29,7 +29,7 @@ import java.util.List;
 
 /**
  * Base class of integration tests with Jersey.
- * 
+ *
  * @author jtremeaux
  */
 public abstract class BaseJerseyTest extends JerseyTest {
@@ -37,7 +37,7 @@ public abstract class BaseJerseyTest extends JerseyTest {
      * Test HTTP server.
      */
     private HttpServer httpServer;
-    
+
     /**
      * Utility class for the REST client.
      */
@@ -46,13 +46,13 @@ public abstract class BaseJerseyTest extends JerseyTest {
     /**
      * Test mail server.
      */
-    private Wiser wiser;
-    
+    //private Wiser wiser;
+
     @Override
     protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
         return new ExternalTestContainerFactory();
     }
-    
+
     @Override
     protected Application configure() {
         String travisEnv = System.getenv("TRAVIS");
@@ -63,12 +63,12 @@ public abstract class BaseJerseyTest extends JerseyTest {
         }
         return new Application();
     }
-    
+
     @Override
     protected URI getBaseUri() {
         return UriBuilder.fromUri(super.getBaseUri()).path("docs").build();
     }
-    
+
     @Override
     @Before
     public void setUp() throws Exception {
@@ -79,7 +79,6 @@ public abstract class BaseJerseyTest extends JerseyTest {
 
         httpServer = HttpServer.createSimpleServer(getClass().getResource("/").getFile(), "localhost", getPort());
         WebappContext context = new WebappContext("GrizzlyContext", "/docs");
-        context.addListener("com.twelvemonkeys.servlet.image.IIOProviderContextListener");
         context.addFilter("requestContextFilter", RequestContextFilter.class)
                 .addMappingForUrlPatterns(null, "/*");
         context.addFilter("tokenBasedSecurityFilter", TokenBasedSecurityFilter.class)
@@ -96,9 +95,9 @@ public abstract class BaseJerseyTest extends JerseyTest {
         context.deploy(httpServer);
         httpServer.start();
 
-        wiser = new Wiser();
-        wiser.setPort(2500);
-        wiser.start();
+//        wiser = new Wiser();
+//        wiser.setPort(2500);
+//        wiser.start();
     }
 
     /**
@@ -106,30 +105,31 @@ public abstract class BaseJerseyTest extends JerseyTest {
      *
      * @return Email content
      * @throws MessagingException e
-     * @throws IOException e
+     * @throws IOException        e
      */
     protected String popEmail() throws MessagingException, IOException {
-        List<WiserMessage> wiserMessageList = wiser.getMessages();
-        if (wiserMessageList.isEmpty()) {
-            return null;
-        }
-        WiserMessage wiserMessage = wiserMessageList.get(wiserMessageList.size() - 1);
-        wiserMessageList.remove(wiserMessageList.size() - 1);
-        MimeMessage message = wiserMessage.getMimeMessage();
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        message.writeTo(os);
-        return os.toString();
+        return null;
+//        List<WiserMessage> wiserMessageList = wiser.getMessages();
+//        if (wiserMessageList.isEmpty()) {
+//            return null;
+//        }
+//        WiserMessage wiserMessage = wiserMessageList.get(wiserMessageList.size() - 1);
+//        wiserMessageList.remove(wiserMessageList.size() - 1);
+//        MimeMessage message = wiserMessage.getMimeMessage();
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//        message.writeTo(os);
+//        return os.toString();
     }
 
     @Override
     @After
     public void tearDown() throws Exception {
-        super.tearDown();
-        if (wiser != null) {
-            wiser.stop();
-        }
-        if (httpServer != null) {
-            httpServer.shutdownNow();
-        }
+//        super.tearDown();
+//        if (wiser != null) {
+//            wiser.stop();
+//        }
+//        if (httpServer != null) {
+//            httpServer.shutdownNow();
+//        }
     }
 }
