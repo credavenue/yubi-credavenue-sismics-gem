4980cd6b497a57154d24be52f77d05a282d408ab
saurabh-yubi <saurabh.verma@go-yubi.com>
2023-05-03 22:16:36 +0530

    t document drop column cmd removed

--- docs-core/src/main/resources/db/update/dbupdate-035-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-035-0.sql
@@ -1,4 +1,3 @@
 ALTER TABLE file_password RENAME TO file_details;
 alter table file_details add column IS_SCANNED BOOLEAN DEFAULT FALSE;
-ALTER TABLE T_DOCUMENT DROP COLUMN IS_SCANNED;
 update T_CONFIG set CFG_VALUE_C = '35' where CFG_ID_C = 'DB_VERSION';
