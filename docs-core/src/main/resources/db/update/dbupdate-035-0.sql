a95b0820b79d237f4a45624db96ec97a459cd6d0
saurabh-yubi <saurabh.verma@go-yubi.com>
2023-05-04 16:18:44 +0530

    file_password column not null removed

--- docs-core/src/main/resources/db/update/dbupdate-035-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-035-0.sql
@@ -1,5 +1,4 @@
-ALTER TABLE file_password RENAME TO file_details;
-alter table file_details add column IS_SCANNED BOOLEAN DEFAULT FALSE;
+--ALTER TABLE file_password RENAME TO file_details;
+--alter table file_details add column IS_SCANNED BOOLEAN DEFAULT FALSE;
 alter table file_details alter column file_password drop not null;
-alter table file_details add column temp varchar;
 update T_CONFIG set CFG_VALUE_C = '35' where CFG_ID_C = 'DB_VERSION';
