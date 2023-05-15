ec182688f55612b2f0e698332acf285111d0e461
saurabh-yubi <saurabh.verma@go-yubi.com>
2023-05-15 15:59:22 +0530

    uncomment changes

--- docs-core/src/main/resources/db/update/dbupdate-035-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-035-0.sql
@@ -1,5 +1,5 @@
---ALTER TABLE file_password RENAME TO file_details;
---alter table file_details add column IS_SCANNED BOOLEAN DEFAULT FALSE;
---alter table file_details alter column file_password drop not null;
+ALTER TABLE file_password RENAME TO file_details;
+alter table file_details add column IS_SCANNED BOOLEAN DEFAULT FALSE;
+alter table file_details alter column file_password drop not null;
 alter table file_details add column reviewer_status_note TEXT;
 update T_CONFIG set CFG_VALUE_C = '35' where CFG_ID_C = 'DB_VERSION';
